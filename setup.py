c45488dfdff04eec43fd2e59fcf9d9cd21b83880
Dean Moldovan <dean0x7d@gmail.com>
2016-06-10 22:37:39 +0200

    Set version info in setup.py
    Uses CXXFLAGS env variable instead of CMAKE_CXX_FLAGS because
    that would clear platform-specific default flags on Windows.

--- setup.py
+++ setup.py
@@ -48,9 +48,12 @@ class CMakeBuild(build_ext):
             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
             build_args += ['--', '-j2']
 
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
         subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
 setup(
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 import cmake_example as m
 
-assert m.__version__ == "dev"
+assert m.__version__ == '0.0.1'
 assert m.add(1, 2) == 3
 assert m.subtract(1, 2) == -1
