d925618392dd42c1610f29b3f9cb68b331176227
Isuru Fernando <isuruf@gmail.com>
2020-06-21 21:10:32 -0500

    `-A x64` is not applicable to all CMake generators
    The CMake Generator can be set using the env variable CMAKE_GENERATOR
    and if NMake or Ninja is set, then this results in an error

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class CMakeBuild(build_ext):
 
         if platform.system() == "Windows":
             cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            if sys.maxsize > 2**32:
+            if sys.maxsize > 2**32 and os.getenv("CMAKE_GENERATOR") is None:
                 cmake_args += ['-A', 'x64']
             build_args += ['--', '/m']
         else:
