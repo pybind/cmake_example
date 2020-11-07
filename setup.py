38d628f812fe1d25dbd0c150ca607b193d580ee7
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2020-11-07 17:59:00 -0500

    fix: try to use compiler discovery

--- setup.py
+++ setup.py
@@ -20,13 +20,15 @@ class CMakeBuild(build_ext):
         try:
             out = subprocess.check_output(["cmake", "--version"])
         except OSError:
-            raise RuntimeError(
-                "CMake must be installed to build the following extensions: "
-                + ", ".join(e.name for e in self.extensions)
+            exts = ", ".join(e.name for e in self.extensions)
+            msg = (
+                "CMake must be installed to build the following extensions: {}".format(
+                    exts
+                )
             )
+            raise RuntimeError(msg)
 
-        for ext in self.extensions:
-            self.build_extension(ext)
+        build_ext.run(self)
 
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
@@ -38,24 +40,31 @@ class CMakeBuild(build_ext):
         # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
         # Using Ninja-build since it a) is available as a wheel and b) multithreads automatically
         cmake_args = [
-            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
-            "-DPYTHON_EXECUTABLE=" + sys.executable,
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}".format(extdir),
+            "-DPYTHON_EXECUTABLE={}".format(sys.executable),
         ]
 
         cfg = "Debug" if self.debug else "Release"
 
-        if sys.platform.startswith("win") and not os.getenv("CMAKE_GENERATOR"):
-            # Only needed for multiplatform generators.
-            # We could use ninja and this would be much simpler if we could
-            # communicate the correct settings to find MSVC reliably.
-            build_args = ['--config', cfg]
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-        else:
-            cmake_args += ["-GNinja", "-DCMAKE_BUILD_TYPE=" + cfg]
+        if self.compiler.compiler_type == "msvc":
+            self.compiler.initialize(self.plat_name)
+            cc = self.compiler.cc.replace("\\", "/")
+            rc = self.compiler.rc.replace("\\", "/")
+            mc = self.compiler.mc.replace("\\", "/")
+            linker = self.compiler.linker.replace("\\", "/")
+            cmake_args += [
+                "-DCMAKE_C_COMPILER={}".format(cc),
+                "-DCMAKE_CXX_COMPILER={}".format(cc),
+                "-DCMAKE_LINKER={}".format(linker),
+                "-DCMAKE_RC_COMPILER={}".format(rc),
+                "-DCMAKE_MC_COMPILER={}".format(mc),
+            ]
 
-        cmake_args += ["-DEXAMPLE_VERSION_INFO=" + self.distribution.get_version()]
+        cmake_args += [
+            "-GNinja",
+            "-DCMAKE_BUILD_TYPE={}".format(cfg),
+            "-DEXAMPLE_VERSION_INFO={}".format(self.distribution.get_version()),
+        ]
 
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
@@ -63,7 +72,7 @@ class CMakeBuild(build_ext):
         subprocess.check_call(
             ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
         )
-        subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=self.build_temp)
+        subprocess.check_call(["cmake", "--build", "."], cwd=self.build_temp)
 
 
 setup(
@@ -74,6 +83,6 @@ setup(
     description="A test project using pybind11 and CMake",
     long_description="",
     ext_modules=[CMakeExtension("cmake_example")],
-    cmdclass={"build_ext":CMakeBuild},
+    cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
 )
