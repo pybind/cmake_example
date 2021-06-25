a7c059c940074b9055393280cf2cf303200f3c5d
Henry Schreiner <HenrySchreinerIII@gmail.com>
2021-06-25 19:28:06 -0400

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import cmake_example as m
 
-assert m.__version__ == "0.0.1"
-assert m.add(1, 2) == 3
-assert m.subtract(1, 2) == -1
+def test_main():
+    assert m.__version__ == "0.0.1"
+    assert m.add(1, 2) == 3
+    assert m.subtract(1, 2) == -1
