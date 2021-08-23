31bc276d91985c9bb94e2b4ec12f3fd528971f2c
Henry Schreiner <HenrySchreinerIII@gmail.com>
2021-08-23 09:45:21 -0700

    fix: pass through CMAKE_ARGS (#58)
    * fix: pass through CMAKE_ARGS
    
    * Update setup.py
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -49,6 +49,10 @@ class CMakeBuild(build_ext):
             "-DCMAKE_BUILD_TYPE={}".format(cfg),  # not used on MSVC, but no harm
         ]
         build_args = []
+        # Adding CMake arguments set as environment variable
+        # (needed e.g. to build for ARM OSx on conda-forge)
+        if "CMAKE_ARGS" in os.environ:
+            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
 
         # In this example, we pass in the version to C++. You might not need to.
         cmake_args += [
