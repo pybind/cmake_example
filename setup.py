f86c430eb8ae8cfe0ad82a8462f9368969bc8f7e
Johnny Hsieh <32300164+mnixry@users.noreply.github.com>
2022-11-17 12:22:38 +0800

    Fix incorrect letter case for Python_EXECUTABLE option

--- setup.py
+++ setup.py
@@ -46,8 +46,8 @@ class CMakeBuild(build_ext):
         # from Python.
         cmake_args = [
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
-            f"-DPYTHON_EXECUTABLE={sys.executable}",
             f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
+            f"-DPython_EXECUTABLE={sys.executable}", # CMake variables are case sensitive
         ]
         build_args = []
         # Adding CMake arguments set as environment variable
